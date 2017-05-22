1e02ca9b263e61704550361c6c9b03e733674a06
Nakama3 <kanoanakama@gmail.com>
2017-05-22 09:12:03 -0700

    Add files via upload

---
+++ Final Project- Atari/Bullets_Enemy.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Bullets_Enemy extends Actor
+{
+    public Bullets_Enemy(){
+        setLocation(Enemy.getx(), Enemy.gety());
+        setDirection(90);
+    }
+    public void act() 
+    {
+        move ();
+    }    
+    public void move(){
+        move(5);
+    }
+}
---
+++ Final Project- Atari/Enemy.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Enemy extends Actor
+{
+    public int x;
+    public int y;
+    public Enemy(int x, int y){
+        setRotation(315);
+        this.x = x;
+        this.y = y;
+    }
+    public void act() 
+    {
+        move();
+    }
+    int getx(){
+        return x;
+    }
+    public void move(){
+       while(getX()!=100&&getY()!=150){
+           move(10);
+           turnTowards(100,150);
+           Greenfoot.delay(10);
+       }
+       for(int i=0; i<5;i++){
+           turn(50);
+           Greenfoot.delay(2);
+        }
+        while(getX()!=50&&getY()!=50){
+           move(10);
+           turnTowards(x,y);
+           Greenfoot.delay(10);
+       }
+       if(getX()==50&&getY()==50){
+            move(0);
+            turnTowards(50,0);
+       }
+    }
+}
