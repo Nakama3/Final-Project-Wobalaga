325edb83d9cc23acab657e2eb4149defae35c248
Nakama3 <kanoanakama@gmail.com>
2017-05-27 15:03:21 -0700

    update 5/27 3:03 pm

--- Final Project- Atari/Background.java
+++ Final Project- Atari/Background.java
@@ -1,21 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Background extends World
 {
-    public Background()
-    {    
+   int row2x = 50;
+   int row1x = 500;
+    public Background(){    
         super(500, 713, 1); 
-        int row2x = 50;
-        int row1x = 500;
         int numofEnemies = 10;
         int Lives = 2;
-        for(int i= 0; i<6; i++){
-            addObject(new Type1(row2x,200), 0, 710);
+        addObject(new Type1(50,50),0, 710);
+   }
+   public void act(){
+      
+    }
+   public void spawn(){
+       for(int i= 0; i<6; i++){
+            addObject(new Type1(row2x,50), 0, 710);
             row2x=row2x+50;
        }
         for(int i= 0; i<4; i++){
-            addObject(new Type1(row1x,400), 500, 710);
+            addObject(new Type1(row1x,100), 500, 710);
             row1x=row1x-50;
        }
         addObject(new Player(), 250,690);
-   }
+    }
 }
--- Final Project- Atari/Enemy.java
+++ Final Project- Atari/Enemy.java
@@ -3,13 +3,12 @@ public abstract class Enemy extends Actor
 {
     public int x;
     public int y;
-    public int w = getWorld().getWidth();
-    public int h = getWorld().getHeight();
-    public char D;
+    public int w = 500;
+    public int h = 712;
     public boolean ready = false;
-    public boolean b = false;
+    public boolean a = false;
     public Enemy(int x, int y){
-        setRotation(315);
+        setRotation(270);
         this.x = x;
         this.y = y;
 
@@ -19,28 +18,29 @@ public abstract class Enemy extends Actor
         spawn(x,y);
         shoot();
     }
-    public void spawn(int x, int y){//(Starting x and y coordinate, and Spawn position(L or R)
-        if(D=='L'){//Left spawn
-            while(getX()!=w/2&&getY()!=h/2){ //Move towards the spin point from spawn
-               move(10);
-               turnTowards(100,150);
-               Greenfoot.delay(10);
+    public void spawn(int x,int y){//(Starting x and y coordinate, and Spawn position(L or R)
+            while(a=false){ //Move towards the spin point from spawn
+               move(20);
+               turnTowards(w/2,h/2);
+               Greenfoot.delay(1);
+               if(getX()==w/2&&getY()==h/2){
+                   a=true;
+                }
            }
-           for(int i=0; i<5;i++){ //spin
+           /*for(int i=0; i<5;i++){ //spin
                turn(50);
                Greenfoot.delay(2);
-            }
+            }*/
             while(getX()!=x&&getY()!=y){ //move into formation postion assigned (x,y)
-               move(10);
+               move(20);
                turnTowards(x,y);
-               Greenfoot.delay(10);
+               Greenfoot.delay(1);
            }
            //if(getX()==x&&getY()==y){ //aim at the bootom of the screen
                 move(0);
                 turnTowards(x,0);
            //}
            ready=true;
-        }
     }
     public void shoot(){
         while(ready==true){
